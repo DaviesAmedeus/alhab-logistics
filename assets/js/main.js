823895b0a66da7d9a20dc6fcc99110b36a019cba
DaviesAmedeus <amedeusedgar@gmail.com>
2025-11-27 16:25:48 +0300

    Complete the the alhab website

---
+++ assets/js/main.js
@@ -0,0 +1,119 @@
+$(function () {
+
+    "use strict";
+
+    //===== Prealoder
+
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+
+
+    //===== Sticky
+
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".header_navbar").removeClass("sticky");
+            $(".header_navbar img").attr("src", "assets/images/alhab_imgs/alhab-logo-white4.png");
+        } else {
+            $(".header_navbar").addClass("sticky");
+            $(".header_navbar img").attr("src", "assets/images/alhab_imgs/alhab-logo-black2.png");
+        }
+    });
+
+
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+
+
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });
+
+
+    //===== Back to top
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+
+
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+
+
+    //=====  WOW active
+
+    var wow = new WOW({
+        boxClass: 'wow', //
+        mobile: false, // 
+    })
+    wow.init();
+
+    //===== 
+
+
+    $("#contact-form").on("submit", function (e) {
+        e.preventDefault(); // Stop form from reloading the page
+
+
+var site_phone = 255752832590
+
+        // form data
+        var name = $("input[name='name']").val();
+        var number = $("input[name='number']").val();
+        var message = $("textarea[name='message']").val();
+
+
+      
+        // Build WhatsApp message
+        var text =
+            "Hello ALHAB 001 LOGISTICS, I am "+ name + message + "This is my number: "+ number;
+
+        // // WhatsApp URL
+        var whatsappURL = "https://wa.me/" + site_phone + "?text=" + text;
+
+        // Open WhatsApp
+        window.open(whatsappURL, "_blank");
+    });
+
+
+
+});
